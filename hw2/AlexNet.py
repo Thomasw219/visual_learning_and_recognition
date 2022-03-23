a7109c13158f61ca0ddcd4ccb29941bb495bb3c6
Raghavvgoel <raghavvg@andrew.cmu.edu>
2022-03-23 13:30:31 -0400

    working 1.6, start 1.7

--- hw2/AlexNet.py
+++ hw2/AlexNet.py
@@ -47,13 +47,33 @@ class LocalizerAlexNetRobust(nn.Module):
     def __init__(self, num_classes=20):
         super(LocalizerAlexNetRobust, self).__init__()
         #TODO: Define model
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), dilation=(1, 1)),
+            nn.Conv2d(64, 192, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), dilation=(1, 1)),
+            nn.Conv2d(192, 384, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+        )
+        self.classifier = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=(1, 1), stride=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(256, 20, kernel_size=(1, 1), stride=(1, 1)),
+            nn.AvgPool2d(3, stride=1, padding=1)
+        )
 
 
     def forward(self, x):
         #TODO: Define fwd pass
-
-
-        return x
+        return self.classifier(self.features(x))
 
 
 def localizer_alexnet(pretrained=False, **kwargs):
@@ -86,6 +106,13 @@ def localizer_alexnet_robust(pretrained=False, **kwargs):
     """
     model = LocalizerAlexNetRobust(**kwargs)
     #TODO: Ignore for now until instructed
-    
+    if pretrained:
+        print("Loading Pretrained Weights")
+        alexnet = models.alexnet(pretrained=True)
+        for i in range(12):
+            model.features[i] = alexnet.features[i]
+
+    for i in [0, 2, 4]:
+        nn.init.xavier_normal_(model.classifier[i].weight)   
 
     return model
